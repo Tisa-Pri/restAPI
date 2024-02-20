f2b2dcd36039dd1fc8f3d16e839adeb8cd70fd01
Tisa-Pri <146384994+Tisa-Pri@users.noreply.github.com>
2024-02-20 13:26:02 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log('test this');
+
+// async/ await 
+async function getPhoto() {
+    try {
+        const datePicker = document.getElementById('date-picker');
+        const selectedDate = datePicker.value;
+
+        // API 
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=8xBW8Ng3m010gifi3J3VTorthslSfa8K8JwdzhcV&date=${selectedDate}`);
+        const data = await response.json();
+        
+        document.body.style.backgroundImage = `url('${data.url}')`;
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundPosition = 'center';
+        document.body.style.backgroundAttachment = 'fixed';
+
+        const photoInfo = document.getElementById('photo-info');
+        photoInfo.innerHTML = `
+            <h2>${data.title}</h2>
+            <p>${data.explanation}</p>
+        `;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        const photoInfo = document.getElementById('photo-info');
+        photoInfo.innerHTML = '<p>Error fetching data. Please try again later.</p>';
+    }
+}
+
+
